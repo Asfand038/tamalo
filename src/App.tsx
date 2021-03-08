0d7336c41c8bcbdc3fe377b7047092396265e3ba
Asfandyar <jamali.asfandyar786@gmail.com>
2021-03-08 16:01:14 +0500

    checking functionality of pre-commit hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      Hello
-      </div>
-      );
+  const d = 'd';
+  return <div>
+    Hello
+    </div>;
 };
 
 export default App;
