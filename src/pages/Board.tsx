9b8722b58f3cfcd3e9fe0dc89ac0158562801245
shaaandi <saad.edtech@gmail.com>
2021-03-13 22:42:53 +0500

    testing lint

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { BoardLayout } from '../layouts';
 
-const BoardPage: React.FC = ({ unused, two }) => {
-  return <BoardLayout></BoardLayout>;
+const BoardPage: React.FC = () => {
+  return <BoardLayout />;
 };
 
 export default BoardPage;
