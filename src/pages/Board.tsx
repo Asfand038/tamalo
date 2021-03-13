f47c4aa0a949606bde005312d483a1a9c39b0a81
shaaandi <saad.edtech@gmail.com>
2021-03-13 22:59:48 +0500

    testing lint

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { BoardLayout } from '../layouts';
 
 const BoardPage: React.FC = () => {
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    setUser(true);
+  }, []);
+
+  // eslint-disable-next-line no-console
+  console.log(user);
+
   return <BoardLayout />;
 };
 
