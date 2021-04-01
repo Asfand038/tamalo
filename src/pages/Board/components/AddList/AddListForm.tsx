d74035453206ba9cebc5caeb94959ffa347594e8
Asfandyar <jamali.asfandyar786@gmail.com>
2021-04-02 00:32:43 +0500

    add click away listeners for add card and add list forms

--- src/pages/Board/components/AddList/AddListForm.tsx
+++ src/pages/Board/components/AddList/AddListForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
+import { ClickAwayListener } from '@material-ui/core';
 
 import { ButtonContainer } from '../../../../components';
 import {
@@ -14,40 +15,21 @@ interface IProps {
 const AddListForm: React.FC<IProps> = ({ setFormIsOpen }) => {
   const [title, setTitle] = useState('');
 
-  const formRef = useRef<HTMLDivElement>(null);
-
-  // Function for closing form if clicked away.
-
-  const closeForm = useCallback(
-    (e: MouseEvent) => {
-      const { current } = formRef;
-      if (current && !current.contains(e.target as HTMLElement)) {
-        setFormIsOpen(false);
-      }
-    },
-    [setFormIsOpen]
-  );
-
-  useEffect(() => {
-    document.addEventListener('mousedown', closeForm);
-    return () => {
-      document.removeEventListener('mousedown', closeForm);
-    };
-  }, [closeForm]);
-
   return (
-    <StyledAddListContainer ref={formRef}>
-      <StyledCard>
-        <StyledListTitleInput
-          placeholder="Enter list title..."
-          variant="outlined"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          autoFocus
-        />
-      </StyledCard>
-      <ButtonContainer setFormIsOpen={setFormIsOpen} btnText="Add list" />
-    </StyledAddListContainer>
+    <ClickAwayListener onClickAway={() => setFormIsOpen(false)}>
+      <StyledAddListContainer>
+        <StyledCard>
+          <StyledListTitleInput
+            placeholder="Enter list title..."
+            variant="outlined"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            autoFocus
+          />
+        </StyledCard>
+        <ButtonContainer setFormIsOpen={setFormIsOpen} btnText="Add list" />
+      </StyledAddListContainer>
+    </ClickAwayListener>
   );
 };
 
--- src/pages/Board/components/InnerList/List/AddCard/OpenedForm.tsx
+++ src/pages/Board/components/InnerList/List/AddCard/OpenedForm.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
-import { Card } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Card, ClickAwayListener } from '@material-ui/core';
 import { MoreHoriz as MoreHorizIcon } from '@material-ui/icons';
 
 import { ButtonContainer } from '../../../../../../components';
@@ -16,44 +16,27 @@ interface IProps {
 const OpenedForm: React.FC<IProps> = ({ setFormIsOpen }) => {
   const [cardTitle, setCardTitle] = useState('');
 
-  const formRef = useRef<HTMLDivElement>(null);
-
-  const closeForm = useCallback(
-    (e: MouseEvent) => {
-      const { current } = formRef;
-      if (current && !current.contains(e.target as HTMLElement)) {
-        setFormIsOpen(false);
-      }
-    },
-    [setFormIsOpen]
-  );
-
-  useEffect(() => {
-    document.addEventListener('mousedown', closeForm);
-    return () => {
-      document.removeEventListener('mousedown', closeForm);
-    };
-  }, [closeForm]);
-
   return (
-    <StyledOpenedFormContainer ref={formRef}>
-      <Card>
-        <StyledTextArea
-          placeholder="Enter a title for this card..."
-          variant="outlined"
-          value={cardTitle}
-          multiline
-          autoFocus
-          rowsMax="8"
-          onChange={(e) => setCardTitle(e.target.value)}
-        />
-      </Card>
-      <ButtonContainer btnText="Add Card" setFormIsOpen={setFormIsOpen}>
-        <StyledOptionsIcon>
-          <MoreHorizIcon />
-        </StyledOptionsIcon>
-      </ButtonContainer>
-    </StyledOpenedFormContainer>
+    <ClickAwayListener onClickAway={() => setFormIsOpen(false)}>
+      <StyledOpenedFormContainer>
+        <Card>
+          <StyledTextArea
+            placeholder="Enter a title for this card..."
+            variant="outlined"
+            value={cardTitle}
+            multiline
+            autoFocus
+            rowsMax="8"
+            onChange={(e) => setCardTitle(e.target.value)}
+          />
+        </Card>
+        <ButtonContainer btnText="Add Card" setFormIsOpen={setFormIsOpen}>
+          <StyledOptionsIcon>
+            <MoreHorizIcon />
+          </StyledOptionsIcon>
+        </ButtonContainer>
+      </StyledOpenedFormContainer>
+    </ClickAwayListener>
   );
 };
 
